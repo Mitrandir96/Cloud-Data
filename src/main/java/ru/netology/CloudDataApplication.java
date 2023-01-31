53733e6a18b2ce6d08a948686c10f61243d2b382
Илья Грачев <grachev.ia96@gmail.com>
2023-01-31 19:43:32 +0700

    initialized project

---
+++ src/main/java/ru/netology/CloudDataApplication.java
@@ -0,0 +1,13 @@
+package ru.netology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudDataApplication.class, args);
+    }
+
+}
