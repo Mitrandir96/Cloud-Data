c0cd5530175c6a989d7ae14d4aed0ae51275b037
Илья Грачев <grachev.ia96@gmail.com>
2023-02-03 00:06:30 +0700

    created UserService layer

---
+++ src/main/java/ru/netology/service/UserService.java
@@ -0,0 +1,25 @@
+package ru.netology.service;
+
+import org.springframework.stereotype.Service;
+import ru.netology.repositories.UserRepository;
+
+import javax.security.auth.login.LoginException;
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public String login(String login, String password) throws LoginException {
+        var optionalUser = userRepository.findUserByLoginAndPasswordHash(login, password);
+        if (!optionalUser.isPresent()) {
+            throw new LoginException("Login and/or password is incorrect.");
+        }
+        var user = optionalUser.get();
+        user.setAuthToken("123");
+        return user.getAuthToken();
+    }
+}
