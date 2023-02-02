a7bbad7d4b6f9595f7fee48a00907f3a808aa8c1
Илья Грачев <grachev.ia96@gmail.com>
2023-02-02 23:53:34 +0700

    added getter and setter in User

--- src/main/java/ru/netology/entities/User.java
+++ src/main/java/ru/netology/entities/User.java
@@ -16,4 +16,43 @@ public class User {
     @OneToMany(mappedBy = "user")
     private Set<File> files;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getAuthToken() {
+        return authToken;
+    }
+
+    public void setAuthToken(String authToken) {
+        this.authToken = authToken;
+    }
+
+    public Set<File> getFiles() {
+        return files;
+    }
+
+    public void setFiles(Set<File> files) {
+        this.files = files;
+    }
 }
