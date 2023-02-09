9c6ce0f6c02988440181dfb45a34195298df3606
Илья Грачев <grachev.ia96@gmail.com>
2023-02-09 16:18:03 +0700

    created ExceptionHandlerAdvice

--- src/main/java/ru/netology/controller/exception/ExceptionHandlerAdvice.java
+++ src/main/java/ru/netology/controller/exception/ExceptionHandlerAdvice.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.netology.dto.GeneralErrorResponse;
@@ -13,14 +14,24 @@ import javax.security.auth.login.LoginException;
 @RestControllerAdvice
 public class ExceptionHandlerAdvice {
     @ExceptionHandler(LoginException.class)
-    public ResponseEntity<String> loginError (LoginException e) {
+    public ResponseEntity<String> onLoginError (LoginException e) {
+        return prepareBadRequestResponseEntity(e.getMessage(), 1);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> onValidationError(MethodArgumentNotValidException e) {
+        return prepareBadRequestResponseEntity(e.getFieldError().getDefaultMessage(), 2);
+    }
+
+    private ResponseEntity<String> prepareBadRequestResponseEntity(String message, int id) {
         var gson = new Gson();
         var err = new GeneralErrorResponse();
-        err.setMessage("Пользователь не найден.");
-        // Указание идентификатора ошибки в моей системе, в данном случае 1 - loginException.
-        err.setId(1);
+        err.setMessage(message);
+        // Указание идентификатора ошибки в моей системе.
+        err.setId(id);
         var ex = gson.toJson(err);
         return ResponseEntity.badRequest().contentType(MediaType.APPLICATION_JSON).body(ex);
+
     }
 
 }
