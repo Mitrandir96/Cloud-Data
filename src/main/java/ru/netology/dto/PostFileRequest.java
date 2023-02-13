cd94d460fb4e72ace9412b6231e98e2f9d0e42ba
Илья Грачев <grachev.ia96@gmail.com>
2023-02-13 23:10:30 +0700

    created post file request

---
+++ src/main/java/ru/netology/dto/PostFileRequest.java
@@ -0,0 +1,14 @@
+package ru.netology.dto;
+
+public class PostFileRequest {
+    private String hash;
+    private String file;
+
+    public String getHash() {
+        return hash;
+    }
+
+    public String getFile() {
+        return file;
+    }
+}
