d8359066174d74571f85eb729e7349e3910f7fe8
Илья Грачев <grachev.ia96@gmail.com>
2023-02-13 23:14:22 +0700

    added post endpoint /file

--- src/main/java/ru/netology/controller/Controller.java
+++ src/main/java/ru/netology/controller/Controller.java
@@ -2,19 +2,24 @@ package ru.netology.controller;
 
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import ru.netology.dto.PostLoginRequest;
 import ru.netology.dto.PostLoginResponse;
+import ru.netology.service.FileService;
 import ru.netology.service.UserService;
 
 import javax.security.auth.login.LoginException;
+import java.io.IOException;
 
 @RestController
 public class Controller {
 
-    private UserService userService;
+    private final UserService userService;
+    private final FileService fileService;
 
-    public Controller(UserService userService) {
+    public Controller(UserService userService, FileService fileService) {
         this.userService = userService;
+        this.fileService = fileService;
     }
 
     @PostMapping("/login")
@@ -23,8 +28,13 @@ public class Controller {
     }
 
     @PostMapping("/logout")
-    public void logout(@RequestHeader("auth-token") String authToken) throws LoginException {
+    public void logout(@RequestHeader("auth-token") String authToken) {
         userService.logout(authToken);
+    }
+
+    @PostMapping("/file")
+    public void uploadFile(@RequestHeader("auth-token") String authToken, @RequestPart String hash, @RequestPart MultipartFile file, @RequestParam String filename) throws IOException, LoginException {
+        fileService.uploadFile(authToken, hash, file, filename);
     }
 
 }
