f3941f30630082623f4233aa2c678a74a0d0ebbd
Илья Грачев <grachev.ia96@gmail.com>
2023-02-13 23:07:50 +0700

    changed field type of file content and added getters, setters

--- src/main/java/ru/netology/entities/File.java
+++ src/main/java/ru/netology/entities/File.java
@@ -10,10 +10,49 @@ public class File {
     private UUID id;
     private String name;
     private String hash;
-    private String content;
+    private byte[] content;
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+
+    public byte[] getContent() {
+        return content;
+    }
+
+    public void setContent(byte[] content) {
+        this.content = content;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
