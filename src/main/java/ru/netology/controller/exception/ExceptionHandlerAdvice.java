39946f701c690867525e0387c31ac56f740de37f
Илья Грачев <grachev.ia96@gmail.com>
2023-02-16 22:29:52 +0700

    refactored method name

--- src/main/java/ru/netology/controller/exception/ExceptionHandlerAdvice.java
+++ src/main/java/ru/netology/controller/exception/ExceptionHandlerAdvice.java
@@ -6,7 +6,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.netology.dto.GeneralErrorResponse;
 
@@ -18,30 +17,30 @@ import java.io.IOException;
 public class ExceptionHandlerAdvice {
     @ExceptionHandler(LoginException.class)
     public ResponseEntity<String> onLoginError (LoginException e) {
-        return prepareBadRequestResponseEntity(e.getMessage(), 1, HttpStatus.BAD_REQUEST);
+        return prepareResponseEntity(e.getMessage(), 1, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<String> onLoginValidationError(MethodArgumentNotValidException e) {
-        return prepareBadRequestResponseEntity(e.getFieldError().getDefaultMessage(), 2, HttpStatus.BAD_REQUEST);
+        return prepareResponseEntity(e.getFieldError().getDefaultMessage(), 2, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(AuthException.class)
     public ResponseEntity<String> authenticationError(AuthException e) {
-        return prepareBadRequestResponseEntity(e.getMessage(), 3, HttpStatus.UNAUTHORIZED);
+        return prepareResponseEntity(e.getMessage(), 3, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> onFileValidationError(IllegalArgumentException e) {
-        return prepareBadRequestResponseEntity(e.getMessage(), 4, HttpStatus.BAD_REQUEST);
+        return prepareResponseEntity(e.getMessage(), 4, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(IOException.class)
     public ResponseEntity<String> getFileBytesError(IOException e) {
-        return prepareBadRequestResponseEntity(e.getMessage(), 5, HttpStatus.BAD_REQUEST);
+        return prepareResponseEntity(e.getMessage(), 5, HttpStatus.BAD_REQUEST);
     }
 
-    private ResponseEntity<String> prepareBadRequestResponseEntity(String message, int id, HttpStatus status) {
+    private ResponseEntity<String> prepareResponseEntity(String message, int id, HttpStatus status) {
         var gson = new Gson();
         var err = new GeneralErrorResponse();
         err.setMessage(message);
