43a1df960e56c3fb5855a43366833e3ab042728d
Илья Грачев <grachev.ia96@gmail.com>
2023-02-18 20:00:01 +0700

    added findFileByNameAndUser method

--- src/main/java/ru/netology/repositories/FileRepository.java
+++ src/main/java/ru/netology/repositories/FileRepository.java
@@ -3,10 +3,13 @@ package ru.netology.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.netology.entities.File;
+import ru.netology.entities.User;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface FileRepository extends JpaRepository<File, UUID> {
+    Optional<File> findFileByNameAndUser(String filename, User user);
 
 }
