2bb239e98a5fbf8c15d60da59bb8a005a51d45fd
Илья Грачев <grachev.ia96@gmail.com>
2023-02-24 18:36:51 +0700

    changed imports

--- src/main/java/ru/netology/service/UserService.java
+++ src/main/java/ru/netology/service/UserService.java
@@ -2,11 +2,8 @@ package ru.netology.service;
 
 import org.springframework.stereotype.Service;
 import ru.netology.dto.PostLoginResponse;
-import ru.netology.entities.File;
-import ru.netology.repositories.FileRepository;
 import ru.netology.repositories.UserRepository;
 import javax.security.auth.login.LoginException;
-import java.util.UUID;
 
 @Service
 public class UserService {
