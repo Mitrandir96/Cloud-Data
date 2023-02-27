07c58225dbadf68134d8a2facc5de14ed8c1dd22
Илья Грачев <grachev.ia96@gmail.com>
2023-02-28 00:41:20 +0700

    added method getAllbyUser

--- src/main/java/ru/netology/repositories/FileRepository.java
+++ src/main/java/ru/netology/repositories/FileRepository.java
@@ -1,15 +1,19 @@
 package ru.netology.repositories;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.netology.entities.File;
 import ru.netology.entities.User;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface FileRepository extends JpaRepository<File, UUID> {
     Optional<File> findFileByNameAndUser(String filename, User user);
+
+    List<File> findAllByUser(User user, Pageable pageable);
 
 }
